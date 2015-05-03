94dd7502bc1dc093a43a60485bfa3922becebf39
Felipe de Araujo <daraujofelipe@gmail.com>
2015-05-03 00:24:48 -0300

    Improve format text

--- app/helpers/protocols_helper.rb
+++ app/helpers/protocols_helper.rb
@@ -1,2 +1,6 @@
 module ProtocolsHelper
+  def shorten_method(text, link)
+    simple_format(truncate(text, length: 460, omission: '... ', separator: ' ') +
+                  link_to('read more', link))
+  end
 end
