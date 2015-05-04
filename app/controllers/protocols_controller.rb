f8e86e280967ae61f57fd1dd04f91bf89ef7d1d9
Felipe de Araujo <daraujofelipe@gmail.com>
2015-05-04 19:13:35 -0300

    Refactor protocol index controller

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -2,7 +2,7 @@ class ProtocolsController < ApplicationController
   before_action :set_protocol, only: [:show, :edit, :update, :destroy]
 
   def index
-    @protocols = Protocol.page(params[:page]).per_page(10).order('created_at DESC')
+    @protocols = Protocol.search(params)
   end
 
   def show
--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,2 +1,10 @@
 class Protocol < ActiveRecord::Base
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
+  def self.search(params)
+    tire.search(load: true, page: params[:page], per_page: 10) do
+      query { string params[:query]} if params[:query].present?
+    end
+  end
 end
