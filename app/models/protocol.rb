c902a2e9bd985c3ca1c5b19b812244f00c0e8c18
Felipe de Araujo <daraujofelipe@gmail.com>
2015-05-06 17:47:11 -0300

    Index Protocol model

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -15,5 +15,17 @@ class Protocol < ActiveRecord::Base
     )
   end
 
-  Protocol.import
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english'
+      indexes :method, analyzer: 'english'
+    end
+  end
 end
+Protocol.__elasticsearch__.client.indices.delete index: Protocol.index_name rescue nil
+ 
+Protocol.__elasticsearch__.client.indices.create \
+  index: Protocol.index_name,
+  body: { settings: Protocol.settings.to_hash, mappings: Protocol.mappings.to_hash }
+ 
+Protocol.import
