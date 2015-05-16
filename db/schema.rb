1f232ab5e0874b709c9da12b9f44567fa3b0c61d
Felipe de Araujo <daraujofelipe@gmail.com>
2015-05-16 17:10:28 -0300

    Add link of source to Protocol

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515174725) do
+ActiveRecord::Schema.define(version: 20150516191525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150515174725) do
     t.string   "plos_id"
     t.string   "alternate_title"
     t.string   "method",          default: [],              array: true
+    t.string   "source"
   end
 
 end
